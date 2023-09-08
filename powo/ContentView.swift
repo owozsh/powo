7b8733df781468e1a1d62b1d3c4727703d9ea484
Vítor Barroso <contact@owozsh.dev>
2023-09-07 21:24:04 -0300

    Initial Commit

---
+++ powo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  powo
+//
+//  Created by Vítor Barroso on 07/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ powo/powoApp.swift
@@ -0,0 +1,17 @@
+//
+//  powoApp.swift
+//  powo
+//
+//  Created by Vítor Barroso on 07/09/23.
+//
+
+import SwiftUI
+
+@main
+struct powoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
